d0c48be1a402c7708eed691bf305ce487d731168
sathinenisnigdhareddy <sathineniandfamily5@gmail.com>
2022-06-26 19:01:40 +0530

    Updates

---
+++ precache-manifest.9aca0622f77805094786e9a2b981d609.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b4b4877cb127d374d2b",
+    "url": "/TextUtils-React/static/css/main.f43bbd2a.chunk.css"
+  },
+  {
+    "revision": "6b4b4877cb127d374d2b",
+    "url": "/TextUtils-React/static/js/main.6b4b4877.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "54d4ef26c5198954da45",
+    "url": "/TextUtils-React/static/js/runtime~main.54d4ef26.js"
+  },
+  {
+    "revision": "5fa60232c3aba7f2da33998d4f48af22",
+    "url": "/TextUtils-React/index.html"
+  }
+];
